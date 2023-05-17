a9dea8fc4ba1ae5aa2a9938853a06034137a4a73
SDK Library <5415409+cauly@users.noreply.github.com>
2023-05-17 17:04:00 +0900

    Create Cauly.xcframework

---
+++ Cauly3.1/CaulyLib/Cauly.xcframework/ios-arm64/Headers/CaulyNativeAdItem.h
@@ -0,0 +1,16 @@
+//
+//  CaulyNativeAdItem.h
+//  Cauly
+//
+//  Created by Neil Kwon on 10/14/15.
+//  Copyright © 2015 Cauly. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CaulyNativeAdItem : NSObject
+
+@property NSString* nativeAdJSONString;
+@property int index;
+
+@end
